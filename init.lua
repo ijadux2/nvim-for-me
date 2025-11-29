b4f427861bb76d725dde3df0e39fab964b735f2e
kumar vaibhav <kv26102009@gmail.com>
2025-11-29 18:53:07 +0530

    neovim + love2d

--- init.lua
+++ init.lua
@@ -623,6 +623,19 @@ require("lazy").setup({
 		end,
 	},
 
+	-- using lazy.nvim
+	{
+		"S1M0N38/love2d.nvim",
+		event = "VeryLazy",
+		version = "2.*",
+		opts = {},
+		keys = {
+			{ "<leader>v", ft = "lua", desc = "LÖVE" },
+			{ "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+			{ "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+		},
+	},
+
 	-- Formatting with conform
 	{
 		"stevearc/conform.nvim",
