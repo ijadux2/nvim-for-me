34a6f313ae7b88f986394f12b0418f821aa68fdc
kumar vaibhav <kv26102009@gmail.com>
2025-12-10 17:09:41 +0530

    nvim reload

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "mesonlsp",
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+    },
+  },
+}
+
